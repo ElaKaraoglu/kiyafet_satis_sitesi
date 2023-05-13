cd240ff711ea2d1a58843577e434b683ae4ad79b
ElaKaraoglu <114621087+ElaKaraoglu@users.noreply.github.com>
2023-05-14 00:50:42 +0300

    Revert "products"
    This reverts commit f7c33a5a9fbce81034e91af7689ec5e6106144ed, reversing
    changes made to 22c0775a577a7e74c3d447349888b325f768168d.

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -28,14 +28,9 @@
 <body>
 
 @include('front.topbar')
-<<<<<<< HEAD
 
 @yield('content')
 @yield('navbar')
-=======
-@include('front.navbar')
-@yield('content')
->>>>>>> b0a1708783c8cc22a04ca962a2b104a0c897c262
 @include('front.footer')
 
 
